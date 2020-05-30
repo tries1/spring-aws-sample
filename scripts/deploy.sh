6c6992df40403e27b9e003748fd48e91d7f2d53b
glenn <glenn.jo@kakaocorp.com>
2020-05-30 23:47:15 +0900

    add aws config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,11 @@
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=spring-aws-sample
 
-cd $REPOSITORY/$PROJECT_NAME/
-
 echo "> 빌드 파일 복사"
-cp /home/ec2-user/app/step1/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 어플리케이션 PID 확인"
-CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl spring-aws-sample | grep jar | awk '{print $1}')
 echo "PID : $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
@@ -20,7 +18,7 @@ else
 fi
 
 echo "새어플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> Jar name : $JAR_NAME"
 echo "> Jar name 에 실행권한 추가"
