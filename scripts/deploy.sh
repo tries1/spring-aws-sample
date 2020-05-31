d159bc8db92c1270115e0ed14ed75cf56bf68f86
glenn <glenn.jo@kakaocorp.com>
2020-05-31 21:11:49 +0900

    modify run jar command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,4 +26,4 @@ echo "> Jar name 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> Jar 실행"
-nohup java -jar -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real.properties -Dspring.profiles.active=real $REPOSITORY/$JAR_NAME 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real.properties -Dspring.profiles.active=real $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
